3f0fcdda0486f5f860c70e1023d8e133713fc184
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-02 03:43:29 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,6 +10,8 @@
   end
 end
 
-def age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
+def age_appropriate_birthday(birthday_kids)}.to output(
+  
+  puts "Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
 
 
