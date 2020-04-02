ff3386bedeb8050e0bca2d9255ed471d7955138f
Rabiah Hogans <hogansrabiah@gmail.com>
2020-04-02 03:36:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,8 +4,12 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  def happy_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  end
 end
+
+def age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
 
 
